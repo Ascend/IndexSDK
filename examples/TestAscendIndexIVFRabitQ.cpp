444153f47ea8275516148b222b9881f8edaacd8c
qanly97 <maqianli20@h-partners.com>
2026-06-18 09:08:26 +0800

    【fix】examples用例更新
    Co-authored-by: qanly97<maqianli20@h-partners.com>

--- examples/TestAscendIndexIVFRabitQ.cpp
+++ examples/TestAscendIndexIVFRabitQ.cpp
@@ -18,7 +18,7 @@
 
 // 需要生成aicpu算子+ivfflat算子+ivfrabitq算子(-d 128 -c 1024)
 
-#include <faiss/ascend/AscendIndexIVFRabitQ.h>
+#include <faiss/ascend/AscendIndexIVFRaBitQ.h>
 
 #include <cfloat>
 #include <cmath>
@@ -48,58 +48,59 @@ void Norm(float *data, size_t n, size_t dim)
             data[i * dim + j] = data[i * dim + j] / l2norm;
         }
     }
+}
 
-    int main()
+int main()
+{
+    size_t dim = 128;
+    size_t ntotal = 100000;
+    int ncentroids = 1024;
+    int nprobe = 32;
+
+    printf("generate data\n");
+    std::vector<float> data(dim * ntotal);
+    for (size_t i = 0; i < data.size(); i++)
     {
-        size_t dim = 128;
-        size_t ntotal = 100000;
-        int ncentroids = 1024;
-        int nprobe = 32;
-
-        printf("generate data\n");
-        std::vector<float> data(dim * ntotal);
-        for (size_t i = 0; i < data.size(); i++)
-        {
-            data[i] = drand48();
-        }
-        Norm(data.data(), ntotal, dim);
-        std::vector<int64_t> ids(ntotal);
-        for (size_t i = 0; i < ids.size(); i++)
-        {
-            ids[i] = i;
-        }
-
-        faiss::ascend::AscendIndexIVFRabitQ *index = nullptr;
-        try
-        {
-            std::vector<int> device{0};
-            int64_t resourceSize = static_cast<int64_t>(2048) * 1024 * 1024;
-            faiss::ascend::AscendIndexIVFRabitQConfig conf(device, resourceSize);
-            conf.useKmeansPP = true;
-            printf("create index\n");
-            index = new faiss::ascend::AscendIndexIVFRabitQ(dim, faiss::MetricType::METRIC_L2, ncentroids, conf);
-            index->verbose = true;
-            index->setNumProbes(nprobe);
-
-            printf("start train\n");
-            index->train(ntotal, data.data());
-            printf("start add\n");
-            index->add_with_ids(ntotal, data.data(), ids.data());
-
-            size_t n = 10;
-            size_t k = 10;
-            std::vector<float> dist(n * k, 0.0);
-            std::vector<faiss::idx_t> label(n * k, 0);
-            printf("start search\n");
-            index->search(n, data.data(), k, dist.data(), label.data());
-        }
-        catch (std::exception &e)
-        {
-            printf("exception caught: %s\n", e.what());
-            delete index;
-            return -1;
-        }
-        delete index;
-        printf("search success\n");
-        return 0;
+        data[i] = drand48();
     }
+    Norm(data.data(), ntotal, dim);
+    std::vector<int64_t> ids(ntotal);
+    for (size_t i = 0; i < ids.size(); i++)
+    {
+        ids[i] = i;
+    }
+
+    faiss::ascend::AscendIndexIVFRaBitQ *index = nullptr;
+    try
+    {
+        std::vector<int> device{0};
+        int64_t resourceSize = static_cast<int64_t>(2048) * 1024 * 1024;
+        faiss::ascend::AscendIndexIVFRaBitQConfig conf(device, resourceSize);
+        conf.useKmeansPP = true;
+        printf("create index\n");
+        index = new faiss::ascend::AscendIndexIVFRaBitQ(dim, faiss::MetricType::METRIC_L2, ncentroids, conf);
+        index->verbose = true;
+        index->setNumProbes(nprobe);
+
+        printf("start train\n");
+        index->train(ntotal, data.data());
+        printf("start add\n");
+        index->add_with_ids(ntotal, data.data(), ids.data());
+
+        size_t n = 10;
+        size_t k = 10;
+        std::vector<float> dist(n * k, 0.0);
+        std::vector<faiss::idx_t> label(n * k, 0);
+        printf("start search\n");
+        index->search(n, data.data(), k, dist.data(), label.data());
+    }
+    catch (std::exception &e)
+    {
+        printf("exception caught: %s\n", e.what());
+        delete index;
+        return -1;
+    }
+    delete index;
+    printf("search success\n");
+    return 0;
+}
